e24ccc8b2631479bce98d9d4173d33aae6576f97
Spooky <157039814+spookykokojunge@users.noreply.github.com>
2024-05-21 17:50:33 +0200

    Update Program.cs
    Removed unused stuff (code cleanup)

--- DmaGlowCS2/Program.cs
+++ DmaGlowCS2/Program.cs
@@ -1,4 +1,4 @@
-﻿using Framework;
+using Framework;
 
 
 //int swed 
@@ -9,7 +9,6 @@ Memory.init("cs2.exe", "client.dll");
 UInt64 dwEntityList = 0x18C7F98;
 
 UInt64 m_hPlayerPawn = 0x7E4;
-UInt64 m_iszPlayerName = 0x638;
 UInt64 m_entitySpottedState = 0x2278;
 UInt64 m_bSpotted = 0x8;
 
@@ -39,17 +38,10 @@ while  (true)
 
         UInt64 currentPawn = Memory.Read<UInt64>(listEntry2 + 0x78 * (pawnHandle & 0x1FF));
 
-        //get pawn attribute, incklude spotted for somereason i cant read this ?
-        //string name = Memory.Read<string>(currentController + m_iszPlayerName + 16);
-
-        bool spotted = Memory.Read<bool>(currentPawn + m_entitySpottedState + m_bSpotted);
-
         //write spotted status
         Memory.Write<bool>(currentPawn + m_entitySpottedState + m_bSpotted, true);
 
-        string spoottedStatus = spotted == true ? "spotted" : " "; //string depending on if entity spotted
 
-        Console.WriteLine($"{spoottedStatus}");
     }
     // sleep a bit 
     Thread.Sleep(100);
